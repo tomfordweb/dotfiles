6d85bb8da00d22039e1b1295494a77f7280e4c64
Tom Ford <tom@datacompusa.com>
2021-07-06 06:18:25 -0400

    Added 2u POS keycaps, add curly braces

--- files/helix/keymap.c
+++ files/helix/keymap.c
@@ -59,7 +59,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC , \
-      ADJUST,  _______, _______, KC_LALT, KC_LGUI, LOWER,   KC_BSPC,  KC_SPC, RAISE,   KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      ADJUST,  _______, KC_LALT, KC_LGUI, LOWER,   _______, KC_BSPC,  KC_SPC, _______,  RAISE,   KC_ENT,  _______, _______, _______ \
+
       ),
 
   [_LOWER] = LAYOUT( \
@@ -72,8 +73,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT( \
       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
       _______, _______, KC_P7,   KC_P8,   KC_P9, _______,                   _______, _______, _______, _______, _______, _______, \
-      _______, _______, KC_P4,   KC_P5,   KC_P6, _______,                   KC_MINS, KC_EQL,  _______,  _______, _______, _______, \
-      _______, _______, KC_P1,   KC_P2,   KC_P3, _______, _______, _______, KC_UNDS, KC_PLUS, _______, _______, _______, _______, \
+      _______, _______, KC_P4,   KC_P5,   KC_P6, _______,                   KC_MINS, KC_EQL,  KC_LCBR,  KC_RCBR, _______, _______, \
+      _______, _______, KC_P1,   KC_P2,   KC_P3, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LPRN, KC_RPRN, _______, _______, \
       _______, _______, KC_P0, _______, _______, _______, KC_DEL, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END \
       ),
   [_ADJUST] =  LAYOUT( \
