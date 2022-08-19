0631ca1c3157105c436e06fbd5d414181384946a
Tom Ford <tom@datacompusa.com>
2022-08-19 08:52:24 -0400

    add some null-ls diagnostics, fix intelephense loading issue

--- roles/role-nvim/files/nvim/lua/user/lsp/null-ls.lua
+++ roles/role-nvim/files/nvim/lua/user/lsp/null-ls.lua
@@ -1,4 +1,5 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+
 if not null_ls_status_ok then
 	return
 end
@@ -11,10 +12,12 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		formatting.prettier.with({ extra_args = { "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
-    diagnostics.php
-    -- diagnostics.flake8
+    diagnostics.eslint,
+    diagnostics.php,
+    diagnostics.phpcs,
+    diagnostics.flake8
 	},
 })
