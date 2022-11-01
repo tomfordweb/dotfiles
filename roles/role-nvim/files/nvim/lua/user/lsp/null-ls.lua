aa703c93fc7d5c2f86b1042ed0dcedbc9ff63a7c
Tom Ford <tom@datacompusa.com>
2022-11-01 08:33:25 -0400

    going to assume i need this

--- roles/role-nvim/files/nvim/lua/user/lsp/null-ls.lua
+++ roles/role-nvim/files/nvim/lua/user/lsp/null-ls.lua
@@ -1,7 +1,7 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -10,14 +10,15 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
+  debug = false,
+  sources = {
+    formatting.prettier.with({ extra_args = { "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.tidy,
+    formatting.stylua,
     diagnostics.eslint,
     diagnostics.php,
-    diagnostics.phpcs,
+    -- diagnostics.phpcs,
     diagnostics.flake8
-	},
+  },
 })
