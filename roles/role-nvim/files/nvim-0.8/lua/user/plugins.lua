74474e07616a02a61bf584d13ceda8e3cb51a84b
Tom Ford <tom@datacompusa.com>
2023-01-18 07:37:14 -0500

    gruvbox baby

--- roles/role-nvim/files/nvim-0.8/lua/user/plugins.lua
+++ roles/role-nvim/files/nvim-0.8/lua/user/plugins.lua
@@ -103,6 +103,7 @@ return packer.startup(function(use)
 
   use "tpope/vim-fugitive"
 
+  use { "ellisonleao/gruvbox.nvim" }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
