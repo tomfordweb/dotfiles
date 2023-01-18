a9bf3a64acaa93fbae54701899bf948a3739cd65
Tom Ford <tom@datacompusa.com>
2023-01-18 07:18:02 -0500

    move over motd

--- roles/role-nvim/files/nvim-0.8/lua/user/alpha.lua
+++ roles/role-nvim/files/nvim-0.8/lua/user/alpha.lua
@@ -5,12 +5,27 @@ end
 
 local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = {
-  [[                               __                ]],
-  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+  [[                _,.-----.,_              ]],
+  [[             ,-~           ~-.           ]],
+  [[            ,^___           ___^.        ]],
+  [[          /~"   ~"   .   "~   "~\        ]],
+  [[         Y  ,--._    I    _.--.  Y       ]],
+  [[          | Y     ~-. | ,-~     Y |      ]],
+  [[          | |        }:{        | |      ]],
+  [[          j l       / | \       ! l      ]],
+  [[       .-~  (__,.--" .^. "--.,__)  ~-.   ]],
+  [[       (           / / | \ \           ) ]],
+  [[       \.____,   ~  \/"\/  ~   .____,/   ]],
+  [[        ^.____                 ____.^    ]],
+  [[           | |T ~\  !   !  /~ T| |       ]],
+  [[           | |l   _ _ _ _ _   !| |       ]],
+  [[           | l \/V V V V V V\/ j |       ]],
+  [[           l  \ \|_|_|_|_|_|/ /  !       ]],
+  [[            \  \[T T T T T TI/  /        ]],
+  [[             \  `^-^-^-^-^-^'  /         ]],
+  [[              \               /          ]],
+  [[               \.           ,/           ]],
+  [[                 "^-.___,-^"             ]],
 }
 dashboard.section.buttons.val = {
   dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
@@ -21,8 +36,13 @@ dashboard.section.buttons.val = {
   dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
+
 local function footer()
-  return "chrisatmachine.com"
+  -- NOTE: requires the fortune-mod package to work
+  local handle = io.popen "fortune"
+  local fortune = handle:read "*a"
+  handle:close()
+  return fortune
 end
 
 dashboard.section.footer.val = footer()
