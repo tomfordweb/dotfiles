773ed26c82f0b43ebc44070299093264f42dcc43
Tom Ford <tomfordweb@gmail.com>
2023-05-09 15:54:11 -0400

    gruvbox is the one true theme

--- roles/role-nvim/files/nvim-0.9/lua/user/colorscheme.lua
+++ roles/role-nvim/files/nvim-0.9/lua/user/colorscheme.lua
@@ -1,11 +1,11 @@
 local M = {
-  "folke/tokyonight.nvim",
-  commit = "e52c41314e83232840d6970e6b072f9fba242eb9",
+  "ellisonleao/gruvbox.nvim",
+  commit = "df149bccb19a02c5c2b9fa6ec0716f0c0487feb0",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
 
-M.name = "tokyonight-night"
+M.name = "gruvbox"
 function M.config()
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
