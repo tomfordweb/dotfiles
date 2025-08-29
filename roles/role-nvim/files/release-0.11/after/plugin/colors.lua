ffa1de9030a9f526ea7fa4533fad20a79a6e820f
Tom Ford <tomfordweb@gmail.com>
2025-08-29 05:40:06 -0400

    cleanup, install nvim0.11

--- roles/role-nvim/files/release-0.11/after/plugin/colors.lua
+++ roles/role-nvim/files/release-0.11/after/plugin/colors.lua
@@ -1,9 +1,8 @@
 function SetColors(color)
-	color = color or "everforest"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  color = color or "everforest"
+  vim.cmd.colorscheme(color)
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 SetColors()
