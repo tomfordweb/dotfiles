6138a5d2f5b5b3dc1e877001ca9d79d31b3d169e
Tom Ford <tom@datacompusa.com>
2025-09-03 08:12:32 -0400

    transparency fixes

--- roles/role-nvim/files/release-0.11/after/plugin/colors.lua
+++ roles/role-nvim/files/release-0.11/after/plugin/colors.lua
@@ -1,8 +1,10 @@
 function SetColors(color)
   color = color or "tokyonight-night"
   vim.cmd.colorscheme(color)
+  -- https://neovim.io/doc/user/syntax.html#_13.-highlight-command
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 end
 
 SetColors()
