2e72759181bf4840f55562e7fb15e87acf3b28e8
Tom Ford <tom@datacompusa.com>
2025-09-06 07:35:04 -0400

    more

--- roles/role-nvim/files/release-0.11/after/plugin/terminal.lua
+++ roles/role-nvim/files/release-0.11/after/plugin/terminal.lua
@@ -1,4 +1,4 @@
-local Terminal  = require('toggleterm.terminal').Terminal
+local Terminal      = require('toggleterm.terminal').Terminal
 
 local generic_float = Terminal:new({
   direction = "float",
@@ -8,7 +8,7 @@ local generic_float = Terminal:new({
   -- function to run on opening the terminal
   on_open = function(term)
     vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
   end,
   -- function to run on closing the terminal
   on_close = function(term)
@@ -16,7 +16,7 @@ local generic_float = Terminal:new({
   end,
 })
 
-local lazygit = Terminal:new({
+local lazygit       = Terminal:new({
   cmd = "lazygit",
   dir = "git_dir",
   direction = "float",
@@ -26,7 +26,8 @@ local lazygit = Terminal:new({
   -- function to run on opening the terminal
   on_open = function(term)
     vim.cmd("startinsert!")
-    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+    -- lazygit uses 'q'
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "Q", "<cmd>close<CR>", { noremap = true, silent = true })
   end,
   -- function to run on closing the terminal
   on_close = function(term)
@@ -42,5 +43,5 @@ function _generic_float_toggle()
   generic_float:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>lua _generic_float_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>lua _generic_float_toggle()<CR>", { noremap = true, silent = true })
